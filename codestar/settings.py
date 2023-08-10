a93e1d5e4dddde351d7abb75be9d8e61599ff21a
RubemJanoni <>
2023-08-10 22:19:58 +0000

    Deployment fixed

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['rubem-codestar2023.herokuapp.com', '8000-rubemjanoni-django-blog-bk83blw5jl.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['rubem-codestar2023.herokuapp.com',
+                 '8000-rubemjanoni-django-blog-bk83blw5jl.us2.codeanyapp.com']
 
 
 # Application definition
@@ -133,7 +134,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static',]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
